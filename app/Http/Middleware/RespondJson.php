3186d9c26846df4877eb6345184d7b491b5a27eb
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-11-18 22:37:17 -0300

    feat(api): create middleware to force app to api

---
+++ app/Http/Middleware/RespondJson.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+
+class RespondJson
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        $request->headers->set('Content-Type', 'application/json');
+
+        $response= $next($request);
+
+        if (!$response instanceof JsonResponse) {
+            $response = $this->factory->json(
+                $response->content(),
+                $response->status(),
+                $response->headers->all()
+            );
+        }
+
+        return $response;
+    }
+}
