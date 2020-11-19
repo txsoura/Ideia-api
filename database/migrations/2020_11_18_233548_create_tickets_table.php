f68c735d979eb2a7d550fae5fac1cccb5b903dcd
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-11-18 22:32:14 -0300

    Feat/vic config jwt (#17)
    * feat(migration): add missing migration
    
    * feat(jwt): configure JWT

---
+++ database/migrations/2020_11_18_233548_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enums\TicketStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('price', 10, 2)->default(0.00);
+            $table->enum('status', TicketStatus::toArray())->default(TicketStatus::PENDENT);
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('event_id')->references('id')->on('events');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
