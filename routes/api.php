2ee21402eccecfdcd0728b08f00cb65ef59c2b6c
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-11-19 19:08:55 -0300

    feat(routes): define api routes

--- routes/api.php
+++ routes/api.php
@@ -33,3 +33,18 @@ Route::group(['prefix' => 'v1/auth', 'middleware' => 'api'], function () {
     Route::get('me', 'Auth\AuthController@me');
     Auth::routes();
 });
+
+Route::group(['prefix' => 'v1', 'middleware' => ['api','jwt.auth']], function () {
+    Route::apiResource('users', 'UserController');
+    Route::apiResource('users/{user}/profile', 'ProfileController');
+    Route::post('users/{user}/profile/upload', 'ProfileController@upload');
+    Route::apiResource('users/{user}/wallets', 'WalletController');
+    Route::put('users/{user}/wallets/{wallet}/in', 'WalletController@in');
+    Route::put('users/{user}/wallets/{wallet}/out', 'WalletController@out');
+
+    // Address
+    Route::apiResource('Address', 'AddressController');
+    Route::apiResource('cities', 'CityController');
+    Route::apiResource('states', 'StateController');
+    Route::apiResource('countries', 'CountryController');
+});
